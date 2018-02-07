d04a592ca8397eecafc8e702174924ddd2003bf6
karmamocro <o.elhajoui@student.fontys.nl>
2018-02-07 22:29:57 +0100

    Added Test Project

---
+++ Algoritmiek-CircusTrein.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Algoritmiek-CircusTrein.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Algoritmiek-CircusTrein.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8834a345-9475-4b3f-908d-d1fdef4c61b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Algoritmiek-CircusTrein.UnitTest/TrainWagonTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algoritmiek_CircusTrein.UnitTest
+{
+    [TestClass]
+    public class TrainWagonTest
+    {
+        [TestMethod]
+        public void AddAnimal_AnimalIsCarnivorAndBigToEmptyWagon_ReturnsTrue()
+        {
+            // Arange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
